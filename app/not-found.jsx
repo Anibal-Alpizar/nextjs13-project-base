b637d062d8fe73267ab7085d03906e73ca8f4888
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-12 09:13:09 -0600

    added: notfound next

---
+++ app/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function NotFound() {
+  return (
+    <section>
+      <h1>404</h1>
+      <p>Page not found</p>
+      <Link href="/">Go back home</Link>
+    </section>
+  );
+}
+
+export default NotFound;
