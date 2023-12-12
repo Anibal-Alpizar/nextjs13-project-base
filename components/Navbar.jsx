106211eb17328e04f14e9f9b5dd8ba973558f723
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-12-11 20:56:35 -0600

    added: next Link

---
+++ components/Navbar.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <h1>Navbar</h1>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/store"> Store</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
